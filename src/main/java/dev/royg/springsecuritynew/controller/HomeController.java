d3e97d7db7333d5c6041448fabb40b15230e29f9
roy <roeder.aringoy@gmail.com>
2022-11-19 07:49:37 +0800

    Initial Commit

---
+++ src/main/java/dev/royg/springsecuritynew/controller/HomeController.java
@@ -0,0 +1,25 @@
+package dev.royg.springsecuritynew.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Hello World";
+    }
+
+    @GetMapping("/users")
+    public String user() {
+        return "Hello User";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello Admin";
+    }
+
+
+}
---
+++ src/test/java/dev/royg/springsecuritynew/SpringSecurityNewApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.royg.springsecuritynew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
